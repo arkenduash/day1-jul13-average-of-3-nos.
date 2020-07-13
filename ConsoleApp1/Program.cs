0faa82e8fa080cdb3d968d0a8be1f5909605f5eb
unknown <arkendu98@gmail.com>
2020-07-13 15:38:07 +0530

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, average;
+
+            Console.Write("Enter 1st number :");
+            number1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter 2nd number :");
+            number2 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter 3rd number :");
+            number3 = Convert.ToInt16(Console.ReadLine());
+
+            average = (number1 + number2 + number3) / 3;
+
+            Console.Write("Average of three numbers is {0}", average);
+
+            Console.ReadKey();
+        }
+    }
+}
